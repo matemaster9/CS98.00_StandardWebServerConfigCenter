80db0edea1b3c9dc390927fb2131c80b2d6e19aa
matemaster <matemaster@163.com>
2022-11-19 20:24:24 +0800

    #test AES算法测试

--- mcc-common/src/test/java/SecurityTest.java
+++ mcc-common/src/test/java/SecurityTest.java
@@ -1,6 +1,11 @@
+import cs.matemaster.standardwebserver.common.model.dto.sys.SysUserDto;
+import cs.matemaster.standardwebserver.common.util.JsonUtil;
 import cs.matemaster.standardwebserver.common.util.SecurityUtil;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 import org.junit.Test;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 /**
@@ -13,5 +18,28 @@ public class SecurityTest {
         Map<String, String> keyPair = SecurityUtil.getRSAKeyPair(SecurityUtil.PasswordLength.Level_1);
         System.out.println(keyPair.get("PublicKeyStr"));
         System.out.println(keyPair.get("PrivateKeyStr"));
+    }
+
+    @Test
+    public void test1() {
+        String aesSecretKey = SecurityUtil.getAESSecretKey(SecurityUtil.PasswordLength.AES_1);
+        System.out.println(aesSecretKey);
+    }
+
+    @Test
+    public void test2() throws DecoderException {
+        SysUserDto sysUser = new SysUserDto();
+        sysUser.setAccount("12968912984");
+        sysUser.setPassword("#skuVJDWO987");
+        String message = JsonUtil.serialize(sysUser);
+
+        String secretKey = SecurityUtil.getAESSecretKey(SecurityUtil.PasswordLength.AES_1);
+
+        String encryptedMessage = SecurityUtil.AESEncrypt(message, secretKey);
+        String decryptedMessage = SecurityUtil.AESDecrypt(encryptedMessage, secretKey);
+
+        System.out.println(encryptedMessage);
+        System.out.println(decryptedMessage);
+        System.out.println(new String(Hex.decodeHex(decryptedMessage), StandardCharsets.UTF_8));
     }
 }
