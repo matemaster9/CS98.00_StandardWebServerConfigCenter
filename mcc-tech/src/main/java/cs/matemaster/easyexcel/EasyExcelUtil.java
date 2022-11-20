8275e78381d10bbf00cb5ae28787b6941baaa789
matemaster <matemaster@163.com>
2022-11-20 21:46:25 +0800

    #test easyexcel写入

---
+++ mcc-tech/src/main/java/cs/matemaster/easyexcel/EasyExcelUtil.java
@@ -0,0 +1,47 @@
+package cs.matemaster.easyexcel;
+
+import com.alibaba.excel.EasyExcelFactory;
+import com.alibaba.excel.context.AnalysisContext;
+import com.alibaba.excel.read.listener.ReadListener;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author matemaster
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class EasyExcelUtil {
+
+    public static <T> List<T> toGenericList(File excel, Class<T> tClass) {
+        List<T> genericList = new ArrayList<>();
+        EasyExcelFactory.read(excel, tClass, new ReadListener<T>() {
+            @Override
+            public void invoke(T data, AnalysisContext analysisContext) {
+                genericList.add(data);
+            }
+
+            @Override
+            public void doAfterAllAnalysed(AnalysisContext analysisContext) {
+
+            }
+        }).sheet().doRead();
+        return genericList;
+    }
+
+    public static void exportExcel(HttpServletResponse response, String filename,List<List<String>> head, List<List<Object>> data) throws IOException {
+        String encodeFilename = URLEncoder.encode(filename, StandardCharsets.UTF_8.name()).replaceAll("\\+", "%20");
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        response.setHeader("Content-disposition", "attachment;filename*=utf-8''" + encodeFilename + ".xlsx");
+
+        EasyExcelFactory.write(response.getOutputStream()).head(head).sheet().doWrite(data);
+    }
+}
--- mcc-tech/src/test/java/EasyExcelRedaTest.java
+++ mcc-tech/src/test/java/EasyExcelRedaTest.java
@@ -1,6 +1,7 @@
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.read.listener.ReadListener;
+import cs.matemaster.easyexcel.EasyExcelUtil;
 import cs.matemaster.easyexcel.PathUtil;
 import cs.matemaster.easyexcel.model.Global500CompanyDto;
 import cs.matemaster.standardwebserver.common.util.JsonUtil;
@@ -60,5 +61,14 @@ public class EasyExcelRedaTest {
         }).sheet().doRead();
 
         System.out.println(JsonUtil.serialize(result));
+    }
+
+    @Test
+    public void test1_v2() {
+        String filename = "/Users/matemaster/Develop/GitHub项目/CS98.00_StandardWebServerConfigCenter/mcc-tech/target/test-classes/全球500强.xlsx";
+        File file = Paths.get(filename).toFile();
+
+        List<Global500CompanyDto> result = EasyExcelUtil.toGenericList(file, Global500CompanyDto.class);
+        result.forEach(System.out::println);
     }
 }
