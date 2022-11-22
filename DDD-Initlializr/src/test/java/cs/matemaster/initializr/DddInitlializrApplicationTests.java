32990089f94709171cd55a2e23596bf4296d0902
matemaster <matemaster@163.com>
2022-11-22 21:41:13 +0800

    #chore ddd

---
+++ DDD-Initlializr/src/test/java/cs/matemaster/initializr/DddInitlializrApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.matemaster.initializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddInitlializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
