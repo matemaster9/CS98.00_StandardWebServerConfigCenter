a9357de2faeecbd637eef53706605dac21449f4f
matemaster <matemaster@163.com>
2022-11-24 20:26:28 +0800

    #feat 信息脱敏

---
+++ mcc-common/src/main/java/cs/matemaster/standardwebserver/common/util/DataMaskingUtil.java
@@ -0,0 +1,31 @@
+package cs.matemaster.standardwebserver.common.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author matemaster
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class DataMaskingUtil {
+
+    public static String desensitizeName(String name) {
+        return null;
+    }
+
+    public static String desensitizeMobile(String mobile) {
+        return null;
+    }
+
+    public static String desensitizeIDCard(String idCard) {
+        return null;
+    }
+
+    public static String desensitizeAddress(String address) {
+        return null;
+    }
+
+    public static String desensitizeBankAccount(String bankAccount) {
+        return null;
+    }
+}
