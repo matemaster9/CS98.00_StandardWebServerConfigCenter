7e7facf6f63bbeb34a3d401707503467234b8de4
matemaster9 <matemaster@163.com>
2022-11-27 20:24:06 +0800

    #feat 七牛云简单上传的凭证

---
+++ mcc-tech/src/main/java/cs/matemaster/tech/qiniuyun/QiNiuYunController.java
@@ -0,0 +1,11 @@
+package cs.matemaster.tech.qiniuyun;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author matemaster
+ */
+@RestController
+public class QiNiuYunController {
+
+}
