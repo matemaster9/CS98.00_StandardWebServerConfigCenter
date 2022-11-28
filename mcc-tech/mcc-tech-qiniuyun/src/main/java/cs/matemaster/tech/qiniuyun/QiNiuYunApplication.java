2f9da3325f35cb28831048c3c749a2ff607a021a
matemaster9 <matemaster@163.com>
2022-11-28 19:21:58 +0800

    #test 上传文件

--- mcc-tech/mcc-tech-qiniuyun/src/main/java/cs/matemaster/tech/qiniuyun/QiNiuYunApplication.java
+++ mcc-tech/mcc-tech-qiniuyun/src/main/java/cs/matemaster/tech/qiniuyun/QiNiuYunApplication.java
@@ -1,5 +1,6 @@
 package cs.matemaster.tech.qiniuyun;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -7,4 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class QiNiuYunApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QiNiuYunApplication.class, args);
+    }
 }
--- mcc-tech/mcc-tech-qiniuyun/src/test/java/QiNiuYunTests.java
+++ mcc-tech/mcc-tech-qiniuyun/src/test/java/QiNiuYunTests.java
@@ -1,9 +1,16 @@
+import com.qiniu.common.QiniuException;
+import com.qiniu.http.Response;
 import com.qiniu.storage.Configuration;
 import com.qiniu.storage.Region;
 import com.qiniu.storage.UploadManager;
+import com.qiniu.storage.model.DefaultPutRet;
 import com.qiniu.util.Auth;
+import cs.matemaster.standardwebserver.common.util.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author matemaster
@@ -45,5 +52,27 @@ public class QiNiuYunTests {
         Region region = Region.region0();
         Configuration configuration = new Configuration(region);
         UploadManager uploadManager = new UploadManager(configuration);
+    }
+
+    @Test
+    public void current() {
+        log.info(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS")));
+    }
+
+    @Test
+    public void upload() throws QiniuException {
+        String file = "/Users/matemaster/Develop/GitHub项目/CS98.00_M1_OOS/mcc-tech/mcc-tech-qiniuyun/src/main/resources/application.yml";
+        String accessKey = "access key";
+        String secretKey = "secret key";
+        String bucket = "bucket name";
+        String key = "file key";
+        Auth auth = Auth.create(accessKey, secretKey);
+        String upToken = auth.uploadToken(bucket, key);
+        Region region = Region.region0();
+        Configuration configuration = new Configuration(region);
+        UploadManager uploadManager = new UploadManager(configuration);
+        Response response = uploadManager.put(file, key, upToken);
+        String responseBodyString = response.bodyString();
+        DefaultPutRet defaultPutRet = JsonUtil.deserialize(responseBodyString, DefaultPutRet.class);
     }
 }
