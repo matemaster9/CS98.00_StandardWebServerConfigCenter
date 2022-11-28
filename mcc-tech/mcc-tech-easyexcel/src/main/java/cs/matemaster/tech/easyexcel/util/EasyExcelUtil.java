ab631705be939666daafc71fb42cd7bf95f14172
matemaster9 <matemaster@163.com>
2022-11-28 21:53:40 +0800

    #style 去除无效代码

--- mcc-tech/mcc-tech-easyexcel/src/main/java/cs/matemaster/tech/easyexcel/util/EasyExcelUtil.java
+++ mcc-tech/mcc-tech-easyexcel/src/main/java/cs/matemaster/tech/easyexcel/util/EasyExcelUtil.java
@@ -2,20 +2,11 @@ package cs.matemaster.tech.easyexcel.util;
 
 import com.alibaba.excel.EasyExcelFactory;
 import com.alibaba.excel.context.AnalysisContext;
-import com.alibaba.excel.enums.CellDataTypeEnum;
-import com.alibaba.excel.metadata.Head;
-import com.alibaba.excel.metadata.data.WriteCellData;
 import com.alibaba.excel.read.listener.ReadListener;
-import com.alibaba.excel.write.handler.context.CellWriteHandlerContext;
-import com.alibaba.excel.write.metadata.holder.WriteSheetHolder;
-import com.alibaba.excel.write.style.column.AbstractColumnWidthStyleStrategy;
-import com.google.common.collect.Maps;
-import cs.matemaster.standardwebserver.common.util.BusinessUtil;
+import com.alibaba.excel.write.style.column.LongestMatchColumnWidthStyleStrategy;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import org.apache.commons.collections4.CollectionUtils;
-import org.apache.poi.ss.usermodel.Cell;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
@@ -24,7 +15,6 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author matemaster
@@ -39,57 +29,17 @@ public final class EasyExcelUtil {
     }
 
     public static void exportExcel(HttpServletResponse response, String filename, List<List<String>> head, List<List<Object>> data) throws IOException {
+
         String encodeFilename = URLEncoder.encode(filename, StandardCharsets.UTF_8.name()).replaceAll("\\+", "%20");
         response.setCharacterEncoding("utf-8");
         response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
         response.setHeader("Content-disposition", "attachment;filename*=utf-8''" + encodeFilename + ".xlsx");
 
-        // todo: 自定义excel列表长宽度
-
-        EasyExcelFactory.write(response.getOutputStream())
+        EasyExcelFactory
+                .write(response.getOutputStream())
                 .head(head)
-                .registerWriteHandler(new AdaptiveColumnWidthStyle())
+                .registerWriteHandler(new LongestMatchColumnWidthStyleStrategy())
                 .sheet().doWrite(data);
-    }
-
-    // todo: 自定义excel列表长宽度
-    private static class AdaptiveColumnWidthStyle extends AbstractColumnWidthStyleStrategy {
-
-        private static final Map<Integer, Map<Integer, Integer>> SheetColumnWithMap = Maps.newHashMapWithExpectedSize(16);
-
-        @Override
-        protected void setColumnWidth(CellWriteHandlerContext context) {
-            if (BusinessUtil.isFalse(context.getHead()) && CollectionUtils.isNotEmpty(context.getCellDataList())) {
-                return;
-            }
-            WriteSheetHolder writeSheetHolder = context.getWriteSheetHolder();
-            Map<Integer, Integer> columnWithMap = SheetColumnWithMap.computeIfAbsent(writeSheetHolder.getSheetNo(), k -> Maps.newHashMap());
-
-        }
-
-        @Override
-        protected void setColumnWidth(WriteSheetHolder writeSheetHolder, List<WriteCellData<?>> cellDataList, Cell cell, Head head, Integer relativeRowIndex, Boolean isHead) {
-            if (BusinessUtil.isFalse(isHead) && CollectionUtils.isNotEmpty(cellDataList)) {
-                return;
-            }
-            Map<Integer, Integer> columnWithMap = SheetColumnWithMap.computeIfAbsent(writeSheetHolder.getSheetNo(), k -> Maps.newHashMap());
-
-        }
-
-        private int columnWidth(List<WriteCellData<?>> data, Cell cell, boolean isHead) {
-            if (isHead) {
-                return cell.getStringCellValue().length();
-            }
-
-            WriteCellData<?> writeCellData = data.get(0);
-            CellDataTypeEnum type = writeCellData.getType();
-            switch (type) {
-                case STRING:
-                    return writeCellData.getStringValue().getBytes(StandardCharsets.UTF_8).length;
-                default:
-                    return String.valueOf(writeCellData.getData()).getBytes(StandardCharsets.UTF_8).length;
-            }
-        }
     }
 
     @AllArgsConstructor
