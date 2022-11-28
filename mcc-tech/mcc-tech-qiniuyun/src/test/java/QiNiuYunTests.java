e6bc1beb3b296dbc469a4d81daa0dd7b9e4aee85
matemaster9 <matemaster@163.com>
2022-11-28 18:55:43 +0800

    #feat 七牛云配置

--- mcc-tech/mcc-tech-qiniuyun/src/test/java/QiNiuYunTests.java
+++ mcc-tech/mcc-tech-qiniuyun/src/test/java/QiNiuYunTests.java
@@ -1,3 +1,6 @@
+import com.qiniu.storage.Configuration;
+import com.qiniu.storage.Region;
+import com.qiniu.storage.UploadManager;
 import com.qiniu.util.Auth;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
@@ -8,6 +11,9 @@ import org.junit.Test;
 @Slf4j
 public class QiNiuYunTests {
 
+    /**
+     * 简单上传凭证
+     */
     @Test
     public void simpleUploadCertificate() {
         String accessKey = "access key";
@@ -17,5 +23,27 @@ public class QiNiuYunTests {
         Auth auth = Auth.create(accessKey, secretKey);
         String upToken = auth.uploadToken(bucket);
         log.debug(upToken);
+    }
+
+    /**
+     * 覆盖文件上传凭证
+     */
+    @Test
+    public void coverUploadCertificate() {
+        String accessKey = "access key";
+        String secretKey = "secret key";
+        String bucket = "bucket name";
+        String key = "file key";
+
+        Auth auth = Auth.create(accessKey, secretKey);
+        String upToken = auth.uploadToken(bucket, key);
+        log.debug(upToken);
+    }
+
+    @Test
+    public void qiNiuYunConfig() {
+        Region region = Region.region0();
+        Configuration configuration = new Configuration(region);
+        UploadManager uploadManager = new UploadManager(configuration);
     }
 }
