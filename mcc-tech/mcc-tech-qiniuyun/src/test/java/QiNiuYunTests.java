4feda19477333d36119484aad2a3383f3d889f6a
matemaster9 <matemaster@163.com>
2022-11-28 20:13:57 +0800

    #test 自定义上传文件凭证返回

--- mcc-tech/mcc-tech-qiniuyun/src/test/java/QiNiuYunTests.java
+++ mcc-tech/mcc-tech-qiniuyun/src/test/java/QiNiuYunTests.java
@@ -5,7 +5,10 @@ import com.qiniu.storage.Region;
 import com.qiniu.storage.UploadManager;
 import com.qiniu.storage.model.DefaultPutRet;
 import com.qiniu.util.Auth;
+import com.qiniu.util.StringMap;
 import cs.matemaster.standardwebserver.common.util.JsonUtil;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
@@ -59,6 +62,11 @@ public class QiNiuYunTests {
         log.info(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS")));
     }
 
+    /**
+     * 上传文件（样例代码）
+     *
+     * @throws QiniuException
+     */
     @Test
     public void upload() throws QiniuException {
         String file = "/Users/matemaster/Develop/GitHub项目/CS98.00_M1_OOS/mcc-tech/mcc-tech-qiniuyun/src/main/resources/application.yml";
@@ -74,5 +82,39 @@ public class QiNiuYunTests {
         Response response = uploadManager.put(file, key, upToken);
         String responseBodyString = response.bodyString();
         DefaultPutRet defaultPutRet = JsonUtil.deserialize(responseBodyString, DefaultPutRet.class);
+    }
+
+    /**
+     * 自定义上传凭证返回
+     */
+    @Test
+    public void returnBody() {
+        String accessKey = "access key";
+        String secretKey = "secret key";
+        String bucket = "bucket name";
+
+        Auth auth = Auth.create(accessKey, secretKey);
+
+        ReturnBody returnBody = new ReturnBody();
+        returnBody.setKey("$(key)");
+        returnBody.setHash("$(etag)");
+        returnBody.setBucket("$(bucket)");
+        returnBody.setFsize("$(fsize)");
+
+        StringMap putPolicy = new StringMap();
+        putPolicy.put("returnBody", JsonUtil.serialize(returnBody));
+
+        String uploadToken = auth.uploadToken(bucket, null, 36000, putPolicy);
+        log.debug(putPolicy.get("returnBody").toString());
+        log.debug(uploadToken);
+    }
+
+    @Setter
+    @Getter
+    private static class ReturnBody {
+        private String key;
+        private String hash;
+        private String bucket;
+        private String fsize;
     }
 }
