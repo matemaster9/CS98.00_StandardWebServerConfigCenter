11bc6cb4120f2c25e12963f39332aac267c6b71d
matemaster9 <matemaster@163.com>
2022-11-29 20:45:10 +0800

    #test jwt签名密钥

--- mcc-tech/mcc-tech-sign/src/test/java/JsonWebTokenTests.java
+++ mcc-tech/mcc-tech-sign/src/test/java/JsonWebTokenTests.java
@@ -8,7 +8,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import javax.crypto.SecretKey;
-import java.util.Collections;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Base64;
 
 /**
  * @author matemaster
@@ -37,5 +41,43 @@ public class JsonWebTokenTests {
         log.debug(claimsJws.getSignature());
         log.debug(claimsJws.getHeader().toString());
         log.debug(claimsJws.getBody().toString());
+    }
+
+    /**
+     * HS256 HMAC
+     * HS384 HMAC
+     * HS512 HMAC
+     * RS256 RSA
+     * RS384 RSA
+     * RS512 RSA
+     * PS256 RSA
+     * PS384 RSA
+     * PS512 RSA
+     * ES256 ECDSA
+     * ES384 ECDSA
+     * ES512 ECDSA
+     */
+    @Test
+    public void test2() {
+        byte[] hs256Key = "U9ISU_9Z$*9o95m@roLuBFcPPC_6&V5y".getBytes(StandardCharsets.UTF_8);
+        SecretKey hs256 = Keys.hmacShaKeyFor(hs256Key);
+        log.debug(hs256.getFormat());
+        log.debug(hs256.getAlgorithm());
+        log.debug(Base64.getEncoder().encodeToString(hs256.getEncoded()));
+        log.debug(new String(hs256.getEncoded(), StandardCharsets.UTF_8));
+        log.debug(hs256.toString());
+
+        KeyPair keyPair = Keys.keyPairFor(SignatureAlgorithm.RS512);
+        PublicKey publicKey = keyPair.getPublic();
+        PrivateKey privateKey = keyPair.getPrivate();
+        log.debug(publicKey.getFormat());
+        log.debug(publicKey.getAlgorithm());
+        log.debug(Base64.getEncoder().encodeToString(publicKey.getEncoded()));
+        log.debug(publicKey.toString());
+
+        log.debug(privateKey.getFormat());
+        log.debug(privateKey.getAlgorithm());
+        log.debug(Base64.getEncoder().encodeToString(privateKey.getEncoded()));
+        log.debug(privateKey.toString());
     }
 }
