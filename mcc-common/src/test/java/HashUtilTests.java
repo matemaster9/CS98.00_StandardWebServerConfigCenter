a1fc9177352d1cbbdcf2c9adbb737149808df056
matemaster9 <matemaster@163.com>
2022-12-03 23:39:23 +0800

    #test hash测试

--- mcc-common/src/test/java/HashUtilTests.java
+++ mcc-common/src/test/java/HashUtilTests.java
@@ -1,4 +1,5 @@
 import cs.matemaster.standardwebserver.common.util.HashUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -6,11 +7,22 @@ import java.util.Arrays;
 /**
  * @author matemaster
  */
+@Slf4j
 public class HashUtilTests {
 
     @Test
     public void test1() {
         long hello = HashUtil.murmurhash3("Hello111");
         System.out.println(hello);
+    }
+
+    @Test
+    public void test2() {
+        log.info(HashUtil.sipHash24AsBase64("Hello World"));
+        log.info(HashUtil.hmacMd5AsBae64("Hello World"));
+        log.info(HashUtil.hmacSHA1AsBase64("Hello World"));
+        log.info(HashUtil.hmacSHA256AsBase64("Hello World"));
+        log.info(HashUtil.hmacSHA512AsBase64("Hello World"));
+        log.info(HashUtil.murmurhash3AsBase64("Hello World"));
     }
 }
