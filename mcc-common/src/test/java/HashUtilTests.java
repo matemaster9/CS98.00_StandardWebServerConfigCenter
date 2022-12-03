3393b33bd8a8b5a3511f00bdd276f0740f99166e
matemaster9 <matemaster@163.com>
2022-12-03 17:29:30 +0800

    #docs 去除废旧代码

--- mcc-common/src/test/java/HashUtilTests.java
+++ mcc-common/src/test/java/HashUtilTests.java
@@ -8,28 +8,4 @@ import java.util.Arrays;
  */
 public class HashUtilTests {
 
-    @Test
-    public void test1() {
-        String message = "Hello World";
-        System.out.println(HashUtil.hmacMd5AsLong(message));
-    }
-
-    @Test
-    public void test2() {
-        byte[] bytes = HashUtil.hmacMD5SecretKeySpecBytes();
-        System.out.println(Arrays.toString(bytes));
-    }
-
-    @Test
-    public void test3() {
-        String message = "Hello World";
-        System.out.println(HashUtil.hmacMd5AsBase64(message));
-    }
-
-    @Test
-    public void test4() {
-        String message = "Hello World";
-        System.out.println(HashUtil.murmur3AsBase64(message));
-        System.out.println(HashUtil.murmur3AsLong(message));
-    }
 }
