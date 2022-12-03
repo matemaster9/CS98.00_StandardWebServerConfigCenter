bff773332bc0247f0b1d7b8e9bc5137207f08cdd
matemaster9 <matemaster@163.com>
2022-12-03 17:30:04 +0800

    #refactor HashUtil结构

--- mcc-common/src/main/java/cs/matemaster/standardwebserver/common/util/HashUtil.java
+++ mcc-common/src/main/java/cs/matemaster/standardwebserver/common/util/HashUtil.java
@@ -1,15 +1,6 @@
 package cs.matemaster.standardwebserver.common.util;
 
-import com.google.common.hash.HashCode;
-import com.google.common.hash.Hashing;
-
 import javax.annotation.Nonnull;
-import javax.crypto.KeyGenerator;
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
-import java.util.Objects;
 
 /**
  * @author matemaster
@@ -19,6 +10,51 @@ public final class HashUtil {
     private HashUtil() {
     }
 
+    public static long hmacMd5(@Nonnull Object arg) {
+        return 0;
+    }
 
+    public static String hmacMd5AsBae64(@Nonnull Object arg) {
+        return "";
+    }
 
+    public static long murmurhash3(@Nonnull Object arg) {
+        return 0;
+    }
+
+    public static String murmurhash3AsBase64(@Nonnull Object arg) {
+        return "";
+    }
+
+    public static long hmacSHA1(@Nonnull Object arg) {
+        return 0;
+    }
+
+    public static String hmacSHA1AsBase64(@Nonnull Object arg) {
+        return "";
+    }
+
+    public static long hmacSHA256(@Nonnull Object arg) {
+        return 0;
+    }
+
+    public static String hmacSHA256AsBase64(@Nonnull Object arg) {
+        return "";
+    }
+
+    public static long hmacSHA512(@Nonnull Object arg) {
+        return 0;
+    }
+
+    public static String hmacSHA512AsBase64(@Nonnull Object arg) {
+        return "";
+    }
+
+    public static long sipHash24(@Nonnull Object arg) {
+        return 0;
+    }
+
+    public static String sipHash24AsBase64(@Nonnull Object arg) {
+        return "";
+    }
 }
