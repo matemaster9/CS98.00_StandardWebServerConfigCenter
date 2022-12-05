8cb9a4ef8b6772b87e002538fe1cefebc2e63ed2
matemaster9 <matemaster@163.com>
2022-12-05 16:50:57 +0800

    #test 简单数据mock

--- mcc-tech/mcc-tech-mock/src/test/java/JavaFakerTests.java
+++ mcc-tech/mcc-tech-mock/src/test/java/JavaFakerTests.java
@@ -1,4 +1,9 @@
+import com.github.javafaker.Address;
 import com.github.javafaker.Faker;
+import com.github.javafaker.IdNumber;
+import com.github.javafaker.Job;
+import com.github.javafaker.Name;
+import com.github.javafaker.PhoneNumber;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
@@ -11,9 +16,47 @@ import java.util.Locale;
 public class JavaFakerTests {
 
     @Test
-    public void test() {
+    public void name() {
         Faker faker = new Faker(Locale.CHINA);
-        String name = faker.name().fullName();
-        log.info(name);
+        Name nameMock = faker.name();
+        log.info(nameMock.name());
+        log.info(nameMock.firstName());
+        log.info(nameMock.lastName());
+        log.info(nameMock.fullName());
+        log.info(nameMock.nameWithMiddle());
+        log.info(nameMock.bloodGroup());
+        log.info(nameMock.username());
+        log.info(nameMock.title());
+    }
+
+    @Test
+    public void phoneNumber() {
+        Faker faker = new Faker(Locale.CHINA);
+        PhoneNumber phoneNumberMock = faker.phoneNumber();
+        log.info(phoneNumberMock.phoneNumber());
+        log.info(phoneNumberMock.cellPhone());
+        log.info(phoneNumberMock.subscriberNumber());
+        log.info(phoneNumberMock.extension());
+    }
+
+    @Test
+    public void idNumber() {
+        Faker faker = new Faker(Locale.CHINA);
+        IdNumber idNumberMock = faker.idNumber();
+        log.info(idNumberMock.valid());
+    }
+
+    @Test
+    public void address() {
+        Faker faker = new Faker(Locale.CHINA);
+        Address addressMock = faker.address();
+        log.info(addressMock.fullAddress());
+    }
+
+    @Test
+    public void job() {
+        Faker faker = new Faker(Locale.CHINA);
+        Job jobMock = faker.job();
+        log.info(jobMock.title());
     }
 }
