3d86f60cfe6a90839bf4228cee23b5269529d44c
matemaster9 <matemaster@163.com>
2022-12-09 20:46:13 +0800

    #fix springboot读取配置文件2

--- mcc-tech/mcc-tech-config/src/main/java/cs/matemaster/tech/config/ConfigApplication.java
+++ mcc-tech/mcc-tech-config/src/main/java/cs/matemaster/tech/config/ConfigApplication.java
@@ -2,13 +2,10 @@ package cs.matemaster.tech.config;
 
 import cs.matemaster.tech.config.model.AppSystemProperties;
 import cs.matemaster.tech.config.model.LoginUserProperties;
-import cs.matemaster.tech.config.model.RandomValueProperties;
-import cs.matemaster.tech.config.model.WebServerProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * @author matemaster
@@ -18,19 +15,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 @ConfigurationPropertiesScan(basePackageClasses = {AppSystemProperties.class, LoginUserProperties.class})
 public class ConfigApplication {
     public static void main(String[] args) {
-        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(ConfigApplication.class, args);
-        WebServerProperties webServerProperties = configurableApplicationContext.getBean(WebServerProperties.class);
-        RandomValueProperties randomValueProperties = configurableApplicationContext.getBean(RandomValueProperties.class);
-        AppSystemProperties appSystemProperties = configurableApplicationContext.getBean(AppSystemProperties.class);
-        LoginUserProperties loginUserProperties = configurableApplicationContext.getBean(LoginUserProperties.class);
-
-        log.info(webServerProperties.toString());
-        log.info(randomValueProperties.toString());
-        log.info(appSystemProperties.toString());
-        log.info(loginUserProperties.toString());
-
-        for (String beanDefinitionName : configurableApplicationContext.getBeanDefinitionNames()) {
-            log.info(beanDefinitionName);
-        }
+        SpringApplication.run(ConfigApplication.class, args);
     }
 }
