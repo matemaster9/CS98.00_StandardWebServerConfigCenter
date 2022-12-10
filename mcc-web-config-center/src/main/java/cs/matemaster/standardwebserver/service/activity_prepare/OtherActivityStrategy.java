82875aa8a7bfc8c40f01eca737694e57ee5e27d5
matemaster9 <matemaster@163.com>
2022-12-10 19:09:39 +0800

    #fix 容器实现策略模式

--- mcc-web-config-center/src/main/java/cs/matemaster/standardwebserver/service/activity_prepare/OtherActivityStrategy.java
+++ mcc-web-config-center/src/main/java/cs/matemaster/standardwebserver/service/activity_prepare/OtherActivityStrategy.java
@@ -1,10 +1,12 @@
 package cs.matemaster.standardwebserver.service.activity_prepare;
 
 import cs.matemaster.standardwebserver.common.model.dto.activity_prepare.ActivityPreparePrincipalDto;
+import org.springframework.stereotype.Component;
 
 /**
  * @author matemaster
  */
+@Component
 public class OtherActivityStrategy extends AbstractActivityPrepareStrategy implements ActivityPrepareService {
     @Override
     protected ActivityPreparePrincipalDto getPrincipal(String personalizedContent) {
