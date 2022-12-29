e4edbc987b7bd44933b0c2c88f8dc449da8eb746
matemaster9 <matemaster@163.com>
2022-12-29 09:56:37 +0800

    #feat mock请求参数校验

--- mcc-common/src/main/java/cs/matemaster/standardwebserver/common/model/request/MockSchemaRequest.java
+++ mcc-common/src/main/java/cs/matemaster/standardwebserver/common/model/request/MockSchemaRequest.java
@@ -1,9 +1,13 @@
 package cs.matemaster.standardwebserver.common.model.request;
 
+import cs.matemaster.standardwebserver.common.constant.MockDataTypeEnum;
+import cs.matemaster.standardwebserver.common.util.BizAssertUtil;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 
@@ -21,7 +25,12 @@ public class MockSchemaRequest {
     private List<Columns> columnsList;
 
     public void validate() {
-
+        BizAssertUtil.illegalParams(StringUtils.isBlank(tableName),"表名不可空");
+        BizAssertUtil.illegalParams(CollectionUtils.isEmpty(columnsList),"列信息不可空");
+        columnsList.forEach(item -> {
+            BizAssertUtil.illegalParams(StringUtils.isBlank(item.getColumnName()),"列名不可空");
+            BizAssertUtil.illegalParams(MockDataTypeEnum.getInstanceByCode(item.getMockType()) == null,"请选择合适模拟类型");
+        });
     }
 
     @Setter
