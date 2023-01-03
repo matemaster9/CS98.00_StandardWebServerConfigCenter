a76dcdb896fdbd2ed3b0a5d39685d3f5ec4407bf
matemaster9 <matemaster@163.com>
2023-01-03 10:46:14 +0800

    #feat es基础设施

--- mcc-tech/mcc-tech-es/src/main/java/cs/matemaster/tech/es/ElasticSearchApplication.java
+++ mcc-tech/mcc-tech-es/src/main/java/cs/matemaster/tech/es/ElasticSearchApplication.java
@@ -1,12 +1,18 @@
 package cs.matemaster.tech.es;
 
+import cs.matemaster.standardwebserver.infrastructure.es.ElasticSearchProps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 /**
  * @author matemaster
  */
-@SpringBootApplication
+@ConfigurationPropertiesScan(basePackageClasses = {ElasticSearchProps.class})
+@SpringBootApplication(scanBasePackages = {
+        "cs.matemaster.standardwebserver.infrastructure.es",
+        "cs.matemaster.tech.es"
+})
 public class ElasticSearchApplication {
     public static void main(String[] args) {
         SpringApplication.run(ElasticSearchApplication.class, args);
