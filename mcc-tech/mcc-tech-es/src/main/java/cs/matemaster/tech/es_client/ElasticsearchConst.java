43716adf3681e8ffae6dccff0c123604232fe768
matemaster9 <matemaster@163.com>
2023-01-03 19:26:01 +0800

    #feat kibana测试数据

---
+++ mcc-tech/mcc-tech-es/src/main/java/cs/matemaster/tech/es_client/ElasticsearchConst.java
@@ -0,0 +1,13 @@
+package cs.matemaster.tech.es_client;
+
+/**
+ * @author matemaster
+ */
+public final class ElasticsearchConst {
+
+    public static final String kibana_sample_data_logs = "kibana_sample_data_logs";
+
+    public static final String kibana_sample_data_ecommerce = "kibana_sample_data_ecommerce";
+
+    public static final String kibana_sample_data_flights = "kibana_sample_data_flights";
+}
