7a9366209d64c5e1e6eb1e8ba8348d5baf483c8b
matemaster9 <matemaster@163.com>
2023-01-06 19:09:29 +0800

    #feat 接口替代可拓展枚举

--- mcc-tech/src/test/java/OperationCase.java
+++ mcc-tech/src/test/java/OperationCase.java
@@ -1,5 +1,8 @@
+import cs.matemaster.effective_java.ExtendOperation;
+import cs.matemaster.effective_java.IOperation;
 import cs.matemaster.effective_java.Operation;
 import cs.matemaster.effective_java.Operation2;
+import cs.matemaster.effective_java.Operation3;
 import cs.matemaster.standardwebserver.common.util.DataFakerUtil;
 import org.junit.Test;
 
@@ -34,6 +37,19 @@ public class OperationCase {
     public void operation2() {
         double apply = Operation2.Modulo.apply(1, 2);
         System.out.println(apply);
+    }
+
+    @Test
+    public void iOperation() {
+        test(Operation3.class, 1, 2);
+        test(ExtendOperation.class, 1, 2);
+    }
+
+    private <T extends Enum<T> & IOperation> void test(Class<T> op, double x, double y) {
+        T[] enumConstants = op.getEnumConstants();
+        for (T enumConstant : enumConstants) {
+            System.out.printf("%f %s %f = %f%n", x, enumConstant, y, enumConstant.apply(x, y));
+        }
     }
 
     @Test
