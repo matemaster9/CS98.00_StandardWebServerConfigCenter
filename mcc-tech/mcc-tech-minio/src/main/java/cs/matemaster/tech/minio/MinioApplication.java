a77d3e2bb8df8fe2aaa0b11dfe48358939496ecf
matemaster9 <matemaster@163.com>
2023-01-18 10:24:08 +0800

    #chore init minio

---
+++ mcc-tech/mcc-tech-minio/src/main/java/cs/matemaster/tech/minio/MinioApplication.java
@@ -0,0 +1,15 @@
+package cs.matemaster.tech.minio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author matemaster
+ */
+@SpringBootApplication
+public class MinioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinioApplication.class, args);
+    }
+}
