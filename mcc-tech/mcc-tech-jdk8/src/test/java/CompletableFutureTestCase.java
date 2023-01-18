38a0fb73e5d20978570b42b67ab5c5b409e61c43
matemaster9 <matemaster@163.com>
2023-01-18 17:10:56 +0800

    #fix CompletableFutureTestCase

--- mcc-tech/mcc-tech-jdk8/src/test/java/CompletableFutureTestCase.java
+++ mcc-tech/mcc-tech-jdk8/src/test/java/CompletableFutureTestCase.java
@@ -1,4 +1,13 @@
+import cs.matemaster.standardwebserver.common.util.DataFakerUtil;
+import lombok.SneakyThrows;
+import lombok.var;
+import org.apache.commons.lang3.RandomUtils;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author matemaster
@@ -6,7 +15,37 @@ import org.junit.Test;
 public class CompletableFutureTestCase {
 
     @Test
-    public void name() {
+    @SneakyThrows
+    public void task() {
 
+        CompletableFuture.runAsync(() -> {
+            byte[] bytes = RandomUtils.nextBytes(10);
+            System.out.println(Arrays.toString(bytes));
+        });
+
+        var future = CompletableFuture.supplyAsync(() -> UUID.randomUUID().toString());
+        System.out.println(future.getNow("UUID"));
+
+        TimeUnit.SECONDS.sleep(1);
+    }
+
+    @Test
+    public void waitTask() {
+        var runAsync1 = CompletableFuture.runAsync(() -> {
+            System.out.println("当前执行线程：" + Thread.currentThread().getName());
+            System.out.println("随机值：" + RandomUtils.nextLong());
+        });
+        var runAsync2 = CompletableFuture.runAsync(() -> {
+            System.out.println("当前执行线程：" + Thread.currentThread().getName());
+            System.out.println("随机值：" + DataFakerUtil.getRandomDeptId());
+        });
+        var runAsync3 = CompletableFuture.runAsync(() -> {
+            System.out.println("当前执行线程：" + Thread.currentThread().getName());
+            System.out.println("随机值：" + RandomUtils.nextDouble());
+        });
+
+        CompletableFuture.allOf(runAsync1, runAsync2, runAsync3).join();
+
+        System.out.println("程序执行完毕");
     }
 }
