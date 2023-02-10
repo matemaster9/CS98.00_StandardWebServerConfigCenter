19306c82906630ba0c28ce33d70fd186c90d061f
matemaster9 <matemaster@163.com>
2023-02-10 14:26:09 +0800

    #feat springboot启动流程探究

--- mcc-tech/mcc-tech-config/src/main/java/cs/matemaster/tech/config/ConfigApplication.java
+++ mcc-tech/mcc-tech-config/src/main/java/cs/matemaster/tech/config/ConfigApplication.java
@@ -3,6 +3,7 @@ package cs.matemaster.tech.config;
 import cs.matemaster.tech.config.model.AppSystemProperties;
 import cs.matemaster.tech.config.model.LoginUserProperties;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -15,6 +16,9 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @ConfigurationPropertiesScan(basePackageClasses = {AppSystemProperties.class, LoginUserProperties.class})
 public class ConfigApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigApplication.class, args);
+//        SpringApplication.run(ConfigApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(ConfigApplication.class);
+        springApplication.setBannerMode(Banner.Mode.OFF);
+        springApplication.run(args);
     }
 }
