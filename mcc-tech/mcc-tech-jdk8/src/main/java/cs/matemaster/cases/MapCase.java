d42e0df4dc58859919c37b2c24ba5383dad95088
matemaster <matemaster@163.com>
2023-03-17 19:31:45 +0800

    treemap

--- mcc-tech/mcc-tech-jdk8/src/main/java/cs/matemaster/cases/MapCase.java
+++ mcc-tech/mcc-tech-jdk8/src/main/java/cs/matemaster/cases/MapCase.java
@@ -1,7 +1,25 @@
 package cs.matemaster.cases;
 
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Comparator;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * @author matemaster
  */
 public class MapCase {
+
+    public static void treeMap() {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        SortedMap<LocalDate, Integer> visitCountMap = new TreeMap<>(Comparator.reverseOrder());
+        LocalDate firstDayOfCurMonth = LocalDate.now().with(TemporalAdjusters.firstDayOfMonth());
+        LocalDate lastDayOfCurMonth = LocalDate.now().with(TemporalAdjusters.lastDayOfMonth());
+        for (LocalDate i = firstDayOfCurMonth; i.isBefore(lastDayOfCurMonth); i = i.plusDays(1)) {
+            visitCountMap.put(i, random.nextInt(0, 10000));
+        }
+        visitCountMap.entrySet().forEach(System.out::println);
+    }
 }
---
+++ mcc-tech/mcc-tech-jdk8/src/test/java/cs/matemaster/test/MapTestCase.java
@@ -0,0 +1,16 @@
+package cs.matemaster.test;
+
+import cs.matemaster.cases.MapCase;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author MateMaster
+ * @since 2023/3/17
+ */
+public class MapTestCase {
+
+    @Test
+    void test() {
+        MapCase.treeMap();
+    }
+}
