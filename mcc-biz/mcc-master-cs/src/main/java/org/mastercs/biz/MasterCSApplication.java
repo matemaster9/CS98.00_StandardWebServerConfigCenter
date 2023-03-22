6c58909b801c71c6578b8bea2172ea4a1d225320
matemaster9 <matemaster@163.com>
2023-03-22 20:10:17 +0800

    MasterCSApplication

---
+++ mcc-biz/mcc-master-cs/src/main/java/org/mastercs/biz/MasterCSApplication.java
@@ -0,0 +1,15 @@
+package org.mastercs.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author matemaster
+ */
+@SpringBootApplication
+public class MasterCSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterCSApplication.class, args);
+    }
+}
