0137c4a5ed8cd6a4e9045e7c54e10df0cb7b3ef7
matemaster9 <matemaster@163.com>
2023-03-25 21:22:05 +0800

    线程交替

---
+++ mcc-biz/mcc-master-cs/src/test/java/ShiftWorkTestCase.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import org.mastercs.biz.cases.ShiftWorkCase;
+
+/**
+ * @author matemaster
+ */
+public class ShiftWorkTestCase {
+
+    @Test
+    void test1() {
+        ShiftWorkCase.printAlternately();
+    }
+}
